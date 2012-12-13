9a701f191ecc465996939069ad982b55ebe7c308
Janic Beauchemin <janic_33@msn.com>
2012-12-13 16:11:01 -0500

    layout update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@
  */
 
 jQuery(document).ready(function() {
-
 	//Lightbox
 	$('#mycarousel a.tip').lightBox({
 		imageLoading: '/images/design/lightbox/lightbox-ico-loading.gif',
@@ -13,6 +12,7 @@ jQuery(document).ready(function() {
 		imageBlank: '/images/design/lightbox/lightbox-blank.gif',
 		txtOf: 'sur'
 	});
+
 	$('#mycarousel2 a.tip').lightBox({
 		imageLoading: '/images/design/lightbox/lightbox-ico-loading.gif',
 		imageBtnClose: '/images/design/lightbox/lightbox-btn-close.gif',
@@ -20,22 +20,6 @@ jQuery(document).ready(function() {
 		imageBtnNext: '/images/design/lightbox/lightbox-btn-next.gif',
 		imageBlank: '/images/design/lightbox/lightbox-blank.gif',
 		txtOf: 'sur'
-	});
-
-	//Hover images social
-	$('#social img').each(function() {
-		var t = $(this);
-		var src1 = t.attr('src'); // initial src
-		var newSrc = src1.substring(src1.lastIndexOf('/'), src1.lastIndexOf('.')); // let's get file name without extension
-		t.hover(function() {
-			$(this).attr('src', '/images/design' + newSrc + '_hover.' + /[^.]+$/.exec(src1)); //last part is for extension
-			$(this).attr('height', '31');
-			$('.imgHoverable').css('padding-top', '0');
-		}, function() {
-			$(this).attr('src', '/images/design' + newSrc + '.' + /[^.]+$/.exec(src1)); //removing '-over' from the name
-			$(this).attr('height', '24');
-			$('.imgHoverable').css('padding-top', '7px');
-		});
 	});
 
 	//tooltip
@@ -52,6 +36,17 @@ jQuery(document).ready(function() {
 	 	infiniteLoop: false,
 	 	hideControlOnEnd: false
 	 });
+
+	//contact_icons animation
+//	$('.contact_icons li').hover(function(){
+//		$(this).animate({
+//			'background-position-y': '0px'
+//		}, 100, 'easeInOutExpo')
+//	}, function() {
+//		$(this).animate({
+//			'background-position-y': '8px'
+//		}, 100, 'easeInOutExpo')
+//	})
 });
 
 
