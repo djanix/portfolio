933c94a8d7008ff129a2019927beca341ca7506c
JanicBeauchemin <janic_33@msn.com>
2013-01-28 17:57:57 -0500

    adding stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,22 @@
-/* Author:
- Janic Beauchemin
- */
-
-jQuery(document).ready(function() {
+$(document).ready(function () {
 	//Lightbox
-	$('#mycarousel a.tip').lightBox({
-		imageLoading: '/images/design/lightbox/lightbox-ico-loading.gif',
-		imageBtnClose: '/images/design/lightbox/lightbox-btn-close.gif',
-		imageBtnPrev: '/images/design/lightbox/lightbox-btn-prev.gif',
-		imageBtnNext: '/images/design/lightbox/lightbox-btn-next.gif',
-		imageBlank: '/images/design/lightbox/lightbox-blank.gif',
-		txtOf: 'sur'
-	});
-
-	$('#mycarousel2 a.tip').lightBox({
-		imageLoading: '/images/design/lightbox/lightbox-ico-loading.gif',
-		imageBtnClose: '/images/design/lightbox/lightbox-btn-close.gif',
-		imageBtnPrev: '/images/design/lightbox/lightbox-btn-prev.gif',
-		imageBtnNext: '/images/design/lightbox/lightbox-btn-next.gif',
-		imageBlank: '/images/design/lightbox/lightbox-blank.gif',
-		txtOf: 'sur'
-	});
+//	$('#mycarousel a.tip').lightBox({
+//		imageLoading: '/images/design/lightbox/lightbox-ico-loading.gif',
+//		imageBtnClose: '/images/design/lightbox/lightbox-btn-close.gif',
+//		imageBtnPrev: '/images/design/lightbox/lightbox-btn-prev.gif',
+//		imageBtnNext: '/images/design/lightbox/lightbox-btn-next.gif',
+//		imageBlank: '/images/design/lightbox/lightbox-blank.gif',
+//		txtOf: 'sur'
+//	});
+//
+//	$('#mycarousel2 a.tip').lightBox({
+//		imageLoading: '/images/design/lightbox/lightbox-ico-loading.gif',
+//		imageBtnClose: '/images/design/lightbox/lightbox-btn-close.gif',
+//		imageBtnPrev: '/images/design/lightbox/lightbox-btn-prev.gif',
+//		imageBtnNext: '/images/design/lightbox/lightbox-btn-next.gif',
+//		imageBlank: '/images/design/lightbox/lightbox-blank.gif',
+//		txtOf: 'sur'
+//	});
 
 	//tooltip
 	// $(".tip").tipTip({
@@ -28,21 +24,24 @@ jQuery(document).ready(function() {
 	// });
 
 	//slider
-	 $('.slider ul').bxSlider({
-	 	easing: 'easeInOutExpo',
-	 	speed: 666,
-	 	displaySlideQty: 4,
-	 	moveSlideQty: 4,
-	 	infiniteLoop: false,
-	 	hideControlOnEnd: false
-	 });
+	$('.slider ul').bxSlider({
+		easing: 'easeInOutExpo',
+		speed: 666,
+		minSlides: 4,
+		maxSlides: 4,
+		slideWidth: 220,
+		slideMargin: 20,
+		pager: false,
+		infiniteLoop: false,
+		hideControlOnEnd: false
+	});
 
 	//contact_icons animation
-	$('.contact_icons li').hover(function(){
+	$('.contact_icons li').hover(function () {
 		$(this).find('img').stop(true, true).animate({
 			paddingTop: '-=10'
 		}, 200, 'easeInOutExpo')
-	}, function() {
+	}, function () {
 		$(this).find('img').stop(true, true).animate({
 			paddingTop: '+=10'
 		}, 200, 'easeInOutExpo')
