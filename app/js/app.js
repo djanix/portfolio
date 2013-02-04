c87d494e920ac29e9b2eb39f41e28c2bf9adac6a
Janic Beauchemin <janic_33@msn.com>
2013-02-04 16:37:16 -0500

    fix bx-slider
    call bxSlider after content is loaded (through a ng-repeat)

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,28 @@
-angular.module('portfolio', []).
-	config(['$routeProvider', function($routeProvider) {
+angular.module('portfolio', [])
+	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
 			when('/', {templateUrl: 'views/home.html',   controller: PortfolioHomeCtrl}).
 			when('/web/:title', {templateUrl: 'views/item-detail.html',   controller: PortfolioWebDetailCtrl}).
 			when('/other/:title', {templateUrl: 'views/item-detail.html',   controller: PortfolioOtherDetailCtrl}).
 			otherwise({redirectTo: '/'});
-	}]);
+	}])
+
+	.directive("checkLast", function() {
+		return function (scope, element) {
+			if (scope.$last === true) {
+				element.ready(function () {
+					$('.slider ul').bxSlider({
+						easing: 'easeInOutExpo',
+						speed: 666,
+						minSlides: 4,
+						maxSlides: 4,
+						slideWidth: 220,
+						slideMargin: 20,
+						pager: false,
+						infiniteLoop: false,
+						hideControlOnEnd: false
+					});
+				})
+			}
+		}
+	});
--- app/js/main.js
+++ app/js/main.js
@@ -23,19 +23,6 @@ $(document).ready(function () {
 	// 	maxWidth: "auto"
 	// });
 
-	//slider
-	$('.slider ul').bxSlider({
-		easing: 'easeInOutExpo',
-		speed: 666,
-		minSlides: 4,
-		maxSlides: 4,
-		slideWidth: 220,
-		slideMargin: 20,
-		pager: false,
-		infiniteLoop: false,
-		hideControlOnEnd: false
-	});
-
 	//contact_icons animation
 	$('.contact_icons li').hover(function () {
 		$(this).find('img').stop(true, true).animate({
