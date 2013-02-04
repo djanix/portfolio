d43f877531bf4c1256e5ccaea9af0ef95d875d77
Janic Beauchemin <janic_33@msn.com>
2013-02-04 15:06:04 -0500

    angular integration

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 angular.module('portfolio', []).
 	config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
-			when('/', {templateUrl: 'views/item-list.html',   controller: PortfolioListCtrl}).
+			when('/', {templateUrl: 'views/home.html',   controller: PortfolioHomeCtrl}).
 			when('/web/:title', {templateUrl: 'views/item-detail.html',   controller: PortfolioWebDetailCtrl}).
 			when('/other/:title', {templateUrl: 'views/item-detail.html',   controller: PortfolioOtherDetailCtrl}).
 			otherwise({redirectTo: '/'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,25 @@
 
 /* Controllers */
 
+function header($scope) {
+	$scope.template = { name: 'header.html', url: 'views/sections/header.html'};
+}
+
+function footer($scope) {
+	$scope.template = { name: 'footer.html', url: 'views/sections/footer.html'};
+}
+
+function preFooter($scope) {
+	$scope.template = { name: 'bottomSection.html', url: 'views/sections/preFooter.html'};
+}
+
 function PortfolioGlobalCtrl($scope, $http) {
 	$http.get('data/i18n/en.json').success(function(data) {
 		$scope.copy = data;
 	});
 }
 
-function PortfolioListCtrl($scope, $http) {
+function PortfolioHomeCtrl($scope, $http) {
 	$http.get('data/work/web.json').success(function(data) {
 		$scope.web = data;
 	});
