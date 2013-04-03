1a67780bf611dcb4bf56ccb47056fb0e14763046
JanicBeauchemin <janic_33@msn.com>
2013-04-02 23:08:08 -0400

    minor update

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,4 +1,4 @@
-app.directive("checkLast", function($timeout) {
+app.directive("loadIsotope", function($timeout) {
 	return function(scope, element) {
 		if (scope.$last !== true) return;
 
@@ -18,23 +18,6 @@ app.directive("checkLast", function($timeout) {
 					$(this).hide();
 				}
 			});
-
-
-
-//			$container.infinitescroll({
-//					navSelector  : '#page_nav',    // selector for the paged navigation
-//					nextSelector : '#page_nav a',  // selector for the NEXT link (to page 2)
-//					itemSelector : '.element',     // selector for all items you'll retrieve
-//					loading: {
-//						finishedMsg: 'No more pages to load.',
-//						img: 'http://i.imgur.com/qkKy8.gif'
-//					}
-//				},
-//				// call Isotope as a callback
-//				function( newElements ) {
-//					$container.isotope( 'appended', $( newElements ) );
-//				}
-//			);
 		}, 500);
 	}
 });
