a0b44f4cb03d375384b8ac623cb2a37f0665b9f9
Janic Beauchemin <janic_33@msn.com>
2014-09-14 18:16:21 -0400

    convert to scss update git ignore list update gruntfile + package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,168 @@
-module.exports = function(grunt) {
-	var portfolioConfig = {
-		app: 'app',
-		jsSrc: 'js/src',
-		jsDest: 'js/dest',
-		cssSrc: 'css/src',
-		cssDest: 'css/dest'
-	};
+module.exports = function (grunt) {
+    require('matchdep').filterDev('grunt-*').forEach(
+        grunt.loadNpmTasks
+    );
 
-	grunt.initConfig({
-		portfolio: portfolioConfig,
-		pkg: grunt.file.readJSON('package.json'),
-		banner: '/* <%= pkg.name %> - <%= pkg.version %>\n' +
-			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-			'* Copyright (c) <%= grunt.template.today("yyyy-mm-dd") %> <%= pkg.author.name %> - <%= pkg.author.url %> */\n\n',
-		jshint: {
-			files: ['<%= portfolio.app %>/<%= portfolio.jsSrc %>/*.js']
-		},
-		less: {
-			options: {
-				yuicompress: true
-			},
-			files: {
-				src: "<%= portfolio.app %>/<%= portfolio.cssSrc %>/style.less",
-				dest: "<%= portfolio.app %>/<%= portfolio.cssDest %>/style.css"
-			}
-		},
-		uglify: {
-			options: {
-				banner: '<%= banner %>',
-				mangle: false,
-				sourceMap: '<%= portfolio.app %>/<%= portfolio.jsDest %>/main.map.js'
-			},
-			target: {
-				src: ['<%= portfolio.app %>/<%= portfolio.jsSrc %>/*.js'],
-				dest: '<%= portfolio.app %>/<%= portfolio.jsDest %>/main.min.js'
-			}
-		},
-		watch: {
-			js: {
-				files: ['<%= portfolio.app %>/<%= portfolio.jsSrc %>/*.js',],
-				tasks: ['jshint', 'uglify']
-			},
-			less: {
-				files: ["<%= portfolio.app %>/css/src/*.less", "<%= portfolio.app %>/css/src/**/*.less"],
-				tasks: ['less']
-			}
-		}
-//		htmlmin: {
-//			options: {
-//				collapseWhitespace: true,
-//				removeComments: true
-//			},
-//			files: {
-//				src: '<%= portfolio.app %>/index.html',
-//				dest: '<%= portfolio.app %>/index.min.html'
-//			}
-//		},
-//		compress: {
-//			main: {
-//				options: {
-//					mode: 'gzip'
-//				},
-//				expand: true,
-//				cwd: '<%= portfolio.app %>/',
-//				src: ['**/*'],
-//				dest: 'public/'
-//			}
-//		},
-	});
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-//	grunt.loadNpmTasks('grunt-contrib-htmlmin');
-//	grunt.loadNpmTasks('grunt-contrib-compress');
+    var config = {
+        vhost: 'basestructure.local',
+        basePath: 'app',
+        jsSrc: '<%= config.basePath %>/assets/js/src',
+        jsDest: '<%= config.basePath %>/assets/js/dest',
+        cssSrc: '<%= config.basePath %>/assets/css/src',
+        cssDest: '<%= config.basePath %>/assets/css/dest',
+        htmlFileExtension: 'html',
+        cacheBreaker: '<%= ((new Date()).valueOf().toString()) + (Math.floor((Math.random()*1000000)+1).toString()) %>',
+        banner: '<%= pkg.name %> - <%= pkg.version %>\n' +
+            '<%= pkg.author.name %> - <%= pkg.author.url %>\n' +
+            'Copyright (c) <%= grunt.template.today("yyyy-mm-dd") %>'
+    };
 
-	grunt.registerTask('default', ['jshint', 'uglify', 'less', 'watch'/*, 'htmlmin', 'compress'*/ ]);
+    grunt.initConfig({
+        config: config,
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '<%= config.banner %>',
+        autoprefixer: {
+            files: {
+                options: {
+                    browsers: [
+                        'last 2 version',
+                        'safari 6',
+                        'ie 9',
+                        'opera 12.1',
+                        'ios 6',
+                        'android 4',
+                        'Firefox ESR'
+                    ],
+                    map: true
+                },
+                expand: true,
+                flatten: true,
+                src: '<%= config.cssDest %>/*.css',
+                dest: '<%= config.cssDest %>'
+            }
+        },
+        browserify: {
+            dist: {
+                files: {
+                    '<%= config.jsDest %>/App.js': '<%= config.jsSrc %>/App.js'
+                },
+                options: {
+                    transform: ['browserify-shim'],
+                    watch: true,
+                    bundleOptions: {
+                        debug: true
+                    }
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : ['<%= config.cssDest %>/**/*.css']
+                },
+                options: {
+                    open: false,
+                    proxy: '<%= config.vhost %>',
+                    watchTask: true
+                }
+            }
+        },
+        csswring: {
+            min: {
+                options: {
+                    map: true,
+                    preserveHacks: true,
+                    banner: '<%= config.banner %>'
+                },
+                expand: true,
+                flatten: true,
+                src: '<%= config.cssDest %>/*.css',
+                dest: '<%= config.cssDest %>/'
+            }
+        },
+        jshint: {
+            files: ['<%= config.jsSrc %>/**/*.js'],
+            options: {
+                force: true
+            }
+        },
+        replace: {
+            cache_break: {
+                src: ['<%= config.basePath %>/*.<%= config.htmlFileExtension %>'],
+                overwrite: true,
+                replacements: [
+                    {
+                        from: /cacheBreak: \".*\"/g,
+                        to: 'cacheBreak: "<%= config.cacheBreaker %>"'
+                    },
+                    {
+                        from: /\.css.*/g,
+                        to: '.css?v=<%= config.cacheBreaker %>"/>'
+                    },
+                    {
+                        from: /\.js.*?"/g,
+                        to: '.js?v=<%= config.cacheBreaker %>"'
+                    }
+                ]
+            },
+            'scss_import_path': {
+                src: ['<%= config.cssDest %>/*.scss'],
+                overwrite: true,
+                replacements: [
+                    {
+                        from: /\\/g,
+                        to: '/'
+                    }
+                ]
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    '<%= config.cssDest %>/main.css': '<%= config.cssDest %>/main.scss'
+                }
+            }
+        },
+        sass_imports: {
+            imports: {
+                src: ['<%= config.cssSrc %>/helpers/vars.scss', '<%= config.cssSrc %>/helpers/*.scss', '<%= config.cssSrc %>/**/*.scss'],
+                dest: '<%= config.cssDest %>/main.scss'
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                    sourceMap: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.jsDest %>',
+                    src: '**/*.js',
+                    dest: '<%= config.jsDest %>/'
+                }]
+            }
+        },
+        watch: {
+            js: {
+                files: ['<%= config.jsSrc %>/**/*.js'],
+                tasks: ['build:js']
+            },
+            sass: {
+                files: ["<%= config.cssSrc %>/**/*.scss"],
+                tasks: ['build:css']
+            },
+            options: {
+                spawn: true
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['build:js', 'build:css', 'replace:cache_break', 'browserSync', 'watch']);
+    grunt.registerTask('deploy', ['build:js', 'build:css', 'replace:cache_break']);
+
+    grunt.registerTask('build:css', ['sass_imports', 'replace:scss_import_path', 'sass', 'autoprefixer', 'csswring']);
+    grunt.registerTask('build:js', ['jshint', 'browserify', 'uglify']);
 };
