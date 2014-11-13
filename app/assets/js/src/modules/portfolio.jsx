01a5755b26971a13a579b2a38611415655a6f9f4
Janic Beauchemin <janic_33@msn.com>
2014-11-13 00:04:17 -0500

    show portfolio items

--- app/assets/js/src/modules/portfolio.jsx
+++ app/assets/js/src/modules/portfolio.jsx
@@ -1,13 +1,41 @@
 var React = require('react');
 
 var Projects = require('./projects');
+var PortfolioItem = require('./portfolioItem');
 
 var Portfolio = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+    },
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: this.props.jsonUrl,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
+        var listPortfolio = this.state.data.map(function(item, i) {
+            return (
+                <PortfolioItem item={item} />
+            );
+        });
+
         return (
             <div>
                 <h2>{this.props.sectionName}</h2>
-                <ul className="portfolioList"></ul>
+
+                <ul className="portfolioList">
+                    {listPortfolio}
+                </ul>
 
                 <h3>Other projects</h3>
                 <Projects />
@@ -17,6 +45,6 @@ var Portfolio = React.createClass({
 });
 
 React.render(
-    <Portfolio sectionName="Portfolio" />,
+    <Portfolio sectionName="Portfolio" jsonUrl="/data/work.json" />,
     document.getElementById('portfolio')
 );
---
+++ app/assets/js/src/modules/portfolioItem.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    render: function() {
+        var imgPath = "/assets/img/portfolio/" + this.props.item.images.thumb;
+        return (
+            <li>
+                <a href={this.props.item.link}>
+                    <img src={imgPath} alt="" height="200" width="350" />
+                </a>
+
+                <div className="info">
+                    <p>{this.props.item.title}</p>
+                    <p className="link">
+                        Link: <a href={this.props.item.link}>{this.props.item.link}</a>
+                    </p>
+                    <p>Date: {this.props.item.date}</p>
+                    <p>While working at: <a href={this.props.item.company.link}>{this.props.item.company.name}</a></p>
+                </div>
+            </li>
+        );
+    }
+});
